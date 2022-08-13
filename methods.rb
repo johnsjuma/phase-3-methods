10233f3388d2078325afaa4873ba286fcf9f666f
jerryjohns <jerry.johns@student.moringaschool.com>
2022-08-13 18:55:47 +0300

    ad

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer 
+
+def greet name
+    puts "Hello, #{name}!"  
+end
+greet "Naureen"
+
+def greet_with_default(default="Naureen")
+    puts "Hello, #{default}!"
+    puts "Hello, programmer!"
+end
+greet_with_default 
+
+def add (num1, num2)
+    return num1 + num2
+end
+
+puts add(1,2)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    number / 2
+end
+puts halve(1)
